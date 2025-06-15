6ccd73cb2a4dfa1f78e402be9b5c6c49892cf9a3
omkar-tibole <162010708+omkar-tibole@users.noreply.github.com>
2025-06-15 22:13:45 +0530

    Add files via upload

---
+++ 1_Variable.py
@@ -0,0 +1,87 @@
+"""
+Variable:
+        Variables is like a container that hold's data
+
+        
+
+Variable Names
+A variable can have a short name (like x and y) or a more descriptive name (age, carname, total_volume).
+
+Rules for Python variables:
+
+A variable name must start with a letter or the underscore character
+A variable name cannot start with a number
+A variable name can only contain alpha-numeric characters and underscores (A-z, 0-9, and _ )
+Variable names are case-sensitive (age, Age and AGE are three different variables)
+A variable name cannot be any of the Python keywords.
+        
+"""
+
+
+# Example
+# Legal variable names:
+
+myvar = "John"
+my_var = "John"
+_my_var = "John"
+myVar = "John"
+MYVAR = "John"
+myvar2 = "John"
+
+
+# Example
+# Illegal variable names:
+"""
+
+2myvar = "John"
+my-var = "John"
+my var = "John"
+
+"""
+
+
+
+# Camel Case
+# Each word, except the first, starts with a capital letter:
+
+myVariableName = "John"
+
+
+
+# Pascal Case
+# Each word starts with a capital letter:
+
+MyVariableName = "John"
+
+
+
+
+# Snake Case
+# Each word is separated by an underscore character:
+
+my_variable_name = "John"
+
+
+
+
+ # this is integer type data
+a=12  
+# We  can Print the type of any operator using type Function
+print(type(a))    
+
+# this is String value
+b="Hello World"  
+print(type(b))
+
+# This is a Boolean Value
+c=True   
+print(type(c))
+
+ # This is a None value
+d=None   
+print(type(d))
+
+
+
+
+
